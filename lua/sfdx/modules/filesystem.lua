8ceeca7a58794a56fcbb62a90e151f8b501907de
Levi Nelson <leviticusnelson@gmail.com>
2022-06-25 19:47:56 -0700

    renamed filesystem again lol

--- lua/sfdx/modules/filesystem.lua
+++ lua/sfdx/modules/filesystem.lua
@@ -1,4 +1,4 @@
-local Filesystem = {}
+local filesystem = {}
 
 local function get_file()
 	local file = vim.api.nvim_buf_get_name(0)
@@ -13,11 +13,11 @@ local function get_file_extension(file)
 	return file:match("[^%.]*$")
 end
 
-Filesystem.get_file_info = function()
+filesystem.get_file_info = function()
 	local file = get_file()
 	local file_name = get_file_name(file)
 	local extension = get_file_extension(file)
 	return { file_name = file_name, extension = extension }
 end
 
-return Filesystem
+return filesystem
